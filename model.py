4599b963429ac580eae79397a5060dea0d38fb5c
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2019-01-05 16:25:43 -0500

    Fix slots

--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 class Report:
-    __slots__ = ['date_reported', 'location', 'case_incidents']
+    __slots__ = ['report_number', 'date_reported', 'location', 'case_incidents']
     def __init__(self, report_number, date_reported, location, case_incidents):
-        self.report_numner = report_number
+        self.report_number = report_number
         self.date_reported = date_reported
         self.location = location
         self.case_incidents = case_incidents
     def __repr__(self):
-        return 'Report(%d, %s, %s, %s)' % (self.report_number, self.date_reported, self.location, self.case_incidents)
+        return 'Report(#%d, %s, %s, %s)' % (self.report_number, self.date_reported, self.location, self.case_incidents)
