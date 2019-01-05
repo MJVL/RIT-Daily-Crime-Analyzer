6d7be8e212552741516150e282ab1872f2cd20b0
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2019-01-05 16:14:01 -0500

    Add report #

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 class Report:
     __slots__ = ['date_reported', 'location', 'case_incidents']
-    def __init__(self, date_reported, location, case_incidents):
+    def __init__(self, report_number, date_reported, location, case_incidents):
+        self.report_numner = report_number
         self.date_reported = date_reported
         self.location = location
         self.case_incidents = case_incidents
     def __repr__(self):
-        return 'Report(%s, %s, %s)' % (self.date_reported, self.location, self.case_incidents)
+        return 'Report(%d, %s, %s, %s)' % (self.report_number, self.date_reported, self.location, self.case_incidents)
