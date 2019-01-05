35ef97c470190a82ed5cc27ccc2dd8fab801ed9f
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2019-01-05 16:14:24 -0500

    Remove pdf_parser lock

--- pdf_parser.py
+++ pdf_parser.py
@@ -10,15 +10,11 @@ def extract_text_from_pdf(pdf_path):
     converter = TextConverter(resource_manager, fake_file_handle)
     page_interpreter = PDFPageInterpreter(resource_manager, converter)
     with open(pdf_path, 'rb') as fh:
-        for page in PDFPage.get_pages(fh, 
-                                      caching=True,
-                                      check_extractable=True):
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
             page_interpreter.process_page(page)
         text = fake_file_handle.getvalue()
     converter.close()
     fake_file_handle.close()
     if text:
         return text
- 
-if __name__ == '__main__':
-    print(extract_text_from_pdf('data/dec_2018.pdf'))
+
