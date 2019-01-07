a16ff11a825982a5f1c500e8a82c252fae8839f3
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2019-01-07 11:45:16 -0500

    Split case incidents into array elements

--- report_generator.py
+++ report_generator.py
@@ -15,7 +15,8 @@ def generate_reports(text):
                  int(re.findall(r'(\d{6})', report)[-1]) if len(re.findall(r'(\d{6})', report)) > 0 else reports[-1].report_number + 1,
                  re.findall(r'\d{2}/\d{2}/\d{2}', report)[0],
                  re.findall(r'(?<=Location\s:\s)(.*)(?=Address)', report)[-1].strip() if len(re.findall(r'(?<=Location\s:\s)(.*)(?=Address)', report)) > 0 else re.findall(r'(?<=\d:\d{2})(.*)(?<=LOT)', report)[-1].strip(),
-                 re.findall(r'(?<=Case\sIncident\(s\):)(.*)(?=Synopsis)', report)[-1].strip())
+                 re.findall(r'(?=[A-z])(.*?)(?=\s-)', re.findall(r'(?<=Case\sIncident\(s\):)(.*)(?=Synopsis)', report)[-1].strip())
+            )
             reports.append(r)
     return reports
 
@@ -24,7 +25,7 @@ def main(pdfs):
     text = {}
     for pdf in pdfs:
         text[pdf] = extract_text_from_pdf('data/' + pdf + '.pdf')
-        print(generate_reports(text[pdf]))
+        generate_reports(text[pdf])
 
 
 if __name__ == '__main__':
